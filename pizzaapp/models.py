d91dbc7c3e77fa7c1de87b6002ee05d4fab15bb1
fasiya <fasiyan7@gmail.com>
2023-09-27 16:42:02 +0530

    Added rest framework

--- pizzaapp/models.py
+++ pizzaapp/models.py
@@ -1,8 +1,15 @@
 from django.db import models
-from .models import UserProfile
+
+'''class Pizzeria(models.Model):
+    address = models.CharField(max_length=512)
+    phone = models.CharField(max_length=40)'''
 
 class Pizza(models.Model):
     title = models.CharField(max_length=120)
     description = models.CharField(max_length=240)
+    '''thumbnail_url = models.URLField()
+    approved = models.BooleanField(default=False)
+    creator = models.ForeignKey(Pizzeria, on_delete=models.CASCADE)
+    '''
     def __str__(self):
         return self.title
