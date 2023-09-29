b5dda4ce022e89e99d643e2afc0953cde6c0a9b2
fasiya <fasiyan7@gmail.com>
2023-09-29 15:05:01 +0530

    update x

--- pizzaapp/models.py
+++ pizzaapp/models.py
@@ -5,14 +5,18 @@ class Pizzeria(models.Model):
     owner = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
     address = models.CharField(max_length=512)
     phone = models.CharField(max_length=40)
+    def __str__(self):
+        return self.owner.user.username
+    
 class Pizza(models.Model):
     title = models.CharField(max_length=120)
     description = models.CharField(max_length=240)
     thumbnail_url = models.URLField(null=True, blank=True)
     approved = models.BooleanField(default=False)
     creator = models.ForeignKey(Pizzeria, null=True, blank=True, on_delete=models.CASCADE)
-
-
+    def __str__(self):
+        return self.title
+    
 
 class Likes(models.Model):
     user = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
